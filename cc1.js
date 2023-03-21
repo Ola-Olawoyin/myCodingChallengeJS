5472d1cb03db9c6396b3f75e702a66d727c24ee6
Bosman <oola.olawoyin@gmail.com>
2023-03-21 13:21:46 +0000

    first commit

---
+++ cc1.js
@@ -0,0 +1,29 @@
+//Test Data 1
+let massJohn = 92;
+let heightJohn = 1.95;
+let heightMark = 1.69;
+let massMark = 78;
+
+let BMIJohn = massJohn/ heightJohn**2;
+let BMIMark = massMark/ heightMark**2;
+
+let markHigherBMI = BMIMark > BMIJohn
+
+console.log (BMIJohn, BMIMark);
+console.log(markHigherBMI)
+
+
+ massJohn = 85;
+ heightJohn = 1.76;
+
+ heightMark = 1.88;
+ massMark = 95;
+
+ BMIJohn = massJohn/ heightJohn**2;
+
+ BMIMark = massMark/ heightMark**2;
+
+markHigherBMI = BMIMark > BMIJohn
+
+console.log (BMIJohn, BMIMark);
+console.log(markHigherBMI)
