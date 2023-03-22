b461eb69f522bd8717d071b093b49576aa05e41a
Bosman <oola.olawoyin@gmail.com>
2023-03-22 11:38:53 +0000

    .

---
+++ cc3_-4.js
@@ -0,0 +1,13 @@
+let noOfScore = 3;
+let dophineScore1 = 96;
+let dophineScore2 =108;
+let dophineScore3 = 89;
+let dolphinAverageScore = (dophineScore1+dophineScore2+dophineScore3)/noOfScore
+
+
+let koalasScore1 = 88;
+let koalasScore2 = 91;
+let koalasScore3 = 110;
+let koalasAverageScore = (koalasScore1+koalasScore2+koalasScore3)/noOfScore
+
+console.log(koalasAverageScore, dolphinAverageScore)
