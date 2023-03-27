a2768556215670caa9f8ac2809769fee7e7e2537
Bosman <oola.olawoyin@gmail.com>
2023-03-27 12:27:30 +0100

    .

--- cc1_2.js
+++ cc1_2.js
@@ -127,3 +127,47 @@ const checkWinner = function (avgDolphin, avgKoalas) {
 checkWinner(averageDolphins, averageKoalas);
 
 
+const mark = {
+    fullName: 'Mark Miller',
+    mass: 78,
+    height:1.69,
+
+    calcBMI:function(){
+        this.bmi = this.mass/this.height**2
+
+        return this.bmi
+    }   
+}
+
+const john = {
+    fullName: 'John Smith',
+    mass: 92,
+    height:1.95,
+
+    calcBMI:function(){
+        this.bmi = this.mass/this.height**2
+
+        return this.bmi
+    }
+}
+
+john.calcBMI();
+mark.calcBMI();
+
+if(john.bmi>mark.bmi){
+    console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s  BMI (${mark.bmi})!`)
+}
+
+else if(mark.bmi>john.bmi)
+{
+    console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s BMI (${john.bmi})!`)
+}
+else
+{
+   console.log(`${mark.fullName}'s BMI (${mark.bmi}) is same with ${john.fullName}'s BMI (${john.bmi})`)
+}
+
+
+
+
+
